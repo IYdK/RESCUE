48f11fc09560db796909fa9c1bdaa1d7f8cd3180
IYdK <24259069+IYdK@users.noreply.github.com>
2021-06-08 12:40:50 +0200

    Updated descriptions

--- R/KDIGO_SCr_scanner_dummy_data.R
+++ R/KDIGO_SCr_scanner_dummy_data.R
@@ -1,5 +1,7 @@
 #' Dummy dataset to test the KDIGO_SCr_scanner function
 #'
+#' All data presented in the KDIGO SCr scanner dummy dataset are entirely fictional and perhaps even nonsensical. They are meant to serve an educational purpose in understanding how the code for AKI diagnosis and staging according to KDIGO AKI guideline criteria, works. Furthermore, while very effort was made to ensure that accurate information was implemented in the code to implement AKI diagnosis and staging according to the KIDIGO AKI guideline serum creatinine criteria, we accept no liability or responsibility to any person or organization as a consequence of any reliance upon the information contained in the code or code use.
+#'
 #' @format A data frame with 297 observations and 4 variables:
 #'
 #' @format \[,1\] - 'admission': admission identification number (integer)
